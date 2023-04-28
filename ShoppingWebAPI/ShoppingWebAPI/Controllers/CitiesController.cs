5f2a6d302dead13d15028e2c7f19aa3a90e96220
felip <felipe12091@hotmail.com>
2023-04-28 11:23:33 -0500

    Class CitiesController.cs created

---
+++ ShoppingWebAPI/ShoppingWebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingWebAPI.DAL;
+using ShoppingWebAPI.DAL.Entities;
+using System.Data.Entity.Infrastructure;
+using DbUpdateException = System.Data.Entity.Infrastructure.DbUpdateException;
+
+namespace ShoppingWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public CitiesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetCityById/{cityId}")]
+        public async Task<ActionResult<City>> GetCityById(Guid? cityId)
+        {
+            var city = await _context.Cities.FirstOrDefaultAsync(c => c.Id == cityId);
+            if (city == null) return NotFound();
+            return Ok(city);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetCitiesByStateId/{stateId}")]
+        public async Task<ActionResult<IEnumerable<City>>> GetCitiesByStateId(Guid? stateId)
+        {
+            var cities = await _context.Cities
+                .Where(s => s.StateId == stateId)
+                .ToListAsync();
+            if (cities == null) return NotFound();
+            return cities;
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("CreateCity")]
+        public async Task<ActionResult> CreateCity(City city, Guid stateId)
+        {
+            try
+            {
+                city.Id = Guid.NewGuid();
+                city.CreatedDate = DateTime.Now;
+                city.StateId = stateId;
+                city.State = await _context.States.FirstOrDefaultAsync(s => s.Id == stateId);
+                city.ModifiedDate = null;
+
+                _context.Cities.Add(city);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException dbUpdateException)
+            {
+                if(dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe en {1}.", city.Name, city.State.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(city);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("EditCity/{cityId}")]
+        public async Task<ActionResult> EditCity(Guid cityId, City city)
+        {
+            try
+            {
+                if (cityId != city.Id) return NotFound("City not found");
+
+                city.ModifiedDate = DateTime.Now;
+
+                _context.Cities.Update(city);
+                await _context.SaveChangesAsync(); //Aqui se hace el update...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe en {1}.", city.Name, city.State.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(city);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("DeleteCity/{cityId}")]
+        public async Task<ActionResult> DeleteCity(Guid? cityId)
+        {
+            if (_context.Cities == null) return Problem("Entity set 'DataBaseContext.States' is null.");
+            var city = await _context.Cities.FirstOrDefaultAsync(c => c.Id == cityId);
+
+            if (city == null) return NotFound("City not found");
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return Ok(String.Format("La ciudad {0} fue eliminado.", city.Name));
+        }
+
+
+
+    }
+}
---
+++ ShoppingWebAPI/ShoppingWebAPI/Controllers/StatesController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingWebAPI.DAL;
+using ShoppingWebAPI.DAL.Entities;
+using System.Data.Entity.Infrastructure;
+using DbUpdateException = System.Data.Entity.Infrastructure.DbUpdateException;
+
+namespace ShoppingWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly DataBaseContext _context;
+
+        public StatesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetStateById/{stateId}")]
+        public async Task<ActionResult<State>> GetStateById(Guid? stateId)
+        {
+            var state = await _context.States.FirstOrDefaultAsync(s => s.Id == stateId); //select * from countries
+            if (state == null) return NotFound();
+            return Ok(state);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetStatesByCountryId/{countryId}")]
+        public async Task<ActionResult<IEnumerable<State>>> GetStatesByCountryId(Guid? countryId)
+        {
+            var states = await _context.States
+                .Where(s => s.CountryId == countryId)
+                .ToListAsync();
+            if (states == null) return NotFound();
+            return states;
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("CreateState")]
+        public async Task<ActionResult> CreateState(State state, Guid countryId)
+        {
+            try
+            {
+                state.Id = Guid.NewGuid();
+                state.CreatedDate = DateTime.Now;
+                state.CountryId = countryId;
+                state.Country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == countryId);
+                state.ModifiedDate = null;
+                state.Cities = new List<City>();
+
+                _context.States.Add(state);
+                await _context.SaveChangesAsync(); //Aqui se hace el insert into...
+            }
+            catch(DbUpdateException dbUpdateException)
+            {
+                if(dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe en {1}.", state.Name, state.Country.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(state);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("EditState/{stateId}")]
+        public async Task<ActionResult> EditState(Guid stateId, State state) //El "?" es para indicar que es nulleable
+        {
+            try
+            {
+                if (stateId != state.Id) return NotFound("State not found");
+
+                state.ModifiedDate = DateTime.Now;
+
+                _context.States.Update(state);
+                await _context.SaveChangesAsync(); //Aqui se hace el update...
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe en {1}.", state.Name, state.Country.Name));
+            }
+            catch (Exception ex)
+            {
+                return Conflict(ex.Message);
+            }
+
+            return Ok(state);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("DeleteState/{stateId}")]
+        public async Task<ActionResult> DeleteState(Guid? stateId)
+        {
+            if (_context.States == null) return Problem("Entity set 'DataBaseContext.States' is null.");
+            var state = await _context.States.FirstOrDefaultAsync(s => s.Id == stateId);
+
+            if (state == null) return NotFound("State not found");
+            _context.States.Remove(state);
+            await _context.SaveChangesAsync();
+
+            return Ok(String.Format("El estado {0} fue eliminado.", state.Name));
+        }
+
+
+
+    }
+}
