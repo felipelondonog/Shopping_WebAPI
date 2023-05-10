7def2e9b9810d130050ef7cd09a0448c3b23c25b
felip <felipe12091@hotmail.com>
2023-05-10 14:52:52 -0500

    Project WebPages with CategoriesController and its view created.

---
+++ WebPagesAPI/WebPages/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingWebAPI.DAL.Entities;
+using System.Text.Json.Serialization;
+
+namespace WebPages.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public CategoriesController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7174/api/Categories/Get";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(json);
+
+            return View(categories);
+        }
+
+        
+    }
+}
